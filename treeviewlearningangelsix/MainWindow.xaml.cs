35e39139804a50d113005b6727a1afc29161b06f
ayush <ayushbite@gmail.com>
2023-06-05 09:54:04 +0530

    Add project files.

---
+++ treeviewlearningangelsix/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace treeviewlearningangelsix
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //when windows first loaded
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())//c drive or edrive
+            {
+                var item = new TreeViewItem();
+                treeviewname.Items.Add(item); //adding all the treeviewitems to the treeview
+
+            }
+
+        }
+    }
+}
